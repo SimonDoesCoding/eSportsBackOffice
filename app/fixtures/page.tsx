dd7dc6663835b58b8bb118a4c2f6e229332368f0
SimonDoesCoding <si-aspinall@hotmail.co.uk>
2026-03-11 18:42:09 +0000

    fix issues

--- app/fixtures/page.tsx
+++ app/fixtures/page.tsx
@@ -20,6 +20,7 @@ export default function FixturesPage() {
 
   const handleScheduleMatch = () => {
     setScheduleFormOpen(true);
+    console.log(editingResult);
   };
 
   const handleScheduleSuccess = () => {
